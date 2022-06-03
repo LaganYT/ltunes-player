11649eeab088214d552bccfd0fd7d09a4e1aeba2
Logan Latham <95306463+LaganYT@users.noreply.github.com>
2022-06-03 11:32:42 -0700

    Delete public directory

--- public/filters/capitalize.filter.js
+++
@@ -1,8 +0,0 @@
-/* global angular */
-var app = angular.module('jukebox');
-
-app.filter('capitalize', function() {
-	return function(token) {
-		return token.charAt(0).toUpperCase() + token.slice(1);
-	};
-});
