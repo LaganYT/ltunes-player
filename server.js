0fefae4bcc65d8e9e2b73f5b0b959f05ed33847a
Logan Latham <95306463+LaganYT@users.noreply.github.com>
2022-06-03 11:33:29 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-require('dotenv').config();
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-
 var env = process.env.NODE_ENV;
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var expressGeoIP = require('express-geoip');
-
-global.Promise = require('bluebird');
 
 require('dotenv').config()
 
@@ -24,14 +17,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(expressGeoIP('US').getCountryCodeMiddleware);
     
 app.use('/api', require('./app/routes'));
 app.use('/', express.static('public'));
 
-require('./app/sockets')(io);
-
 var port = process.env.PORT || 8080;
-server.listen(port, function () {
+app.listen(port, function () {
   console.log('App listening on port '+port+'!');
-});
+})
