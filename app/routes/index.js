551dcf04425c6d8e93e592ffae10fc4a25ba24e7
Logan Latham <95306463+LaganYT@users.noreply.github.com>
2022-06-06 21:57:41 -0700

    Add files via upload

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+router.use('/users', require('./user.routes'));
+router.use('/search', require('./search.routes'));
+router.use('/albums', require('./album.routes'));
+router.use('/tracks', require('./track.routes'));
+router.use('/radio', require('./radio.routes'));
+
+module.exports = router;
