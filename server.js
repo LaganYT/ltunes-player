1fa378fae063e970be6e7ba6950d70aef2484331
Logan Latham <95306463+LaganYT@users.noreply.github.com>
2022-06-07 17:14:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -24,5 +24,4 @@ app.use('/', express.static('public'));
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log('App listening on port '+port+'!');
-	console.log('Build run successfully')
 })
